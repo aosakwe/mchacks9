c35fe07c3d8cd5d42febbdb8c781dcbd85a7e512
aosakwe <adrien.osakwe@mail.mcgill.ca>
2022-01-21 22:22:45 -0500

    Initial commit

---
+++ data_clean.R
@@ -0,0 +1,17 @@
+#Generating Formatted CSV file
+library(tidyverse)
+
+
+strsplit(raw[1,],split = " ")
+raw <- read.csv("data.csv")
+
+head(raw)
+
+out <- data.frame(matrix(data = NA, ncol = 3,nrow = 171/3))
+out$X1 <- raw[seq(1,nrow(raw),by=3),]
+out$X2 <- raw[seq(2,nrow(raw),by=3),]
+out$X3 <- raw[seq(3,nrow(raw),by=3),]
+head(out)
+colnames(out) <- c("Station","Arrival Time", "# Passengers")
+
+write.csv(out,file = "clean_data.csv",row.names = FALSE)
